3e485e3c2d89f69b7d70f9223758e619b63fd677
Divya Chhabra <divichhabra49@gmail.com>
2025-06-11 21:48:15 +0530

    Complete

--- script.js
+++ script.js
@@ -376,7 +376,7 @@ window.addEventListener('resize', () => {
     
     // Adjust controls for different screen sizes
     if (window.innerWidth <= 768) {
-        speedControls.style.maxHeight = '40vh';
+        speedControls.style.maxHeight = '50vh';
     } else {
         speedControls.style.position = 'fixed';
         speedControls.style.top = '10px';
